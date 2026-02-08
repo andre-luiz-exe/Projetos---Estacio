afbb3287555473b0ce1caae92a8fbe278ed98182
André Luiz.exe <andrecontapessoal99@gmail.com>
2026-02-08 15:48:22 +0000

    Adicionado Densidade e PIB per Capita

--- super_trunfo.c
+++ super_trunfo.c
@@ -5,13 +5,9 @@ int main(){
     setlocale(LC_ALL, "PT_BR.UTF-8"); //Recurso para a ultilização da biblioteca <locale.h>.
 
     //Declaração das Variáveis para os atributos das cartas.
-    char estado;
-    char cod[03];
-    char cidade;
-    int populacao;
-    float area;
-    float pib;
-    int ponto_turismo;
+    char estado, cod[3], cidade;
+    int populacao, ponto_turismo;
+    float area, pib, densidade, pib_per;
 
     //Começo do Jogo, Cadastrando a Carta1.
     printf("Carta 1:\n");
@@ -35,10 +31,18 @@ int main(){
 
     printf("PIB (bilhões): ", pib);
     scanf("%f", &pib);
-    printf("R$%.3f bilhões de reais\n", pib);
+    printf("R$%.2f bilhões de reais\n", pib);
 
     printf("Números de Pontos Turísticos: ", ponto_turismo);
     scanf("%d", &ponto_turismo);
+
+    //Calculando a Densidade e o PIB per Capita da Carta1.
+    densidade = (float) (populacao / area) * 1000; //Multipliquei por 1000 pois, ao digitar a população e a área em valor inteiro (sem pontos ou vírgulas) o resultado da densidade sairia incorreto.
+    printf("A densidade populacional é: %.0f (habitantes/km²) \n", densidade);
+
+    //Calculando o PIB per Capita.
+    pib_per = (float) (pib / populacao) * 1000; //Multipliquei por 1000 pois, ao digitar o PIB e a população em valor inteiro (sem pontos ou vírgulas) o resultado do PIB per Capita sairia incorreto.
+    printf("O PIB per Capita é: R$%.2f \n", pib_per);
     printf("\n");
 
     //Cadastrando a Carta2.
@@ -53,11 +57,11 @@ int main(){
     printf("Nome da Cidade: ", cidade);
     scanf("%s", &cidade);
 
-    printf("População (milhões):", populacao);
+    printf("População (milhões): ", populacao);
     scanf("%d", &populacao);
     printf("%d milhões de habitantes.\n", populacao);
 
-    printf("Área (mil/Km²)", area);
+    printf("Área (mil/Km²): ", area);
     scanf("%f", &area);
     printf("%.3f mil Km².\n", area);
 
@@ -67,6 +71,14 @@ int main(){
 
     printf("Números de Pontos Turísticos: ", ponto_turismo);
     scanf("%d", &ponto_turismo);
+
+    //Calculando a Densidade e o PIB per Capita da Carta2.
+    densidade = (float) (populacao / area) * 1000; //Multipliquei por 1000 pois, ao digitar a população e a área em valor inteiro (sem pontos ou vírgulas) o resultado da densidade sairia incorreto.
+    printf("A densidade populacional é: %.0f (habitantes/km²) \n", densidade);
+
+    //Calculando o PIB per Capita.
+    pib_per = (float) (pib / populacao) * 1000; //Multipliquei por 1000 pois, ao digitar o PIB e a população em valor inteiro (sem pontos ou vírgulas) o resultado do PIB per Capita sairia incorreto.
+    printf("O PIB per Capita é: R$%.2f \n", pib_per);
 
     //Finalização do Programa.
     return 0;
